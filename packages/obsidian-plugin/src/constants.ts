ebcfacd21661e71707255de76c9ee61a325025d5
onikun94 <onikun94@gmail.com>
2025-04-17 22:52:34 +0900

    chore: 環境変数の設定を改善

--- packages/obsidian-plugin/src/constants.ts
+++ packages/obsidian-plugin/src/constants.ts
@@ -1,4 +1,11 @@
-const BASE_URL = process.env.OBSIDIAN_LINE_API_URL
+const isLocalMode = process.env.NODE_ENV === 'local';
+
+const BASE_URL = process.env.OBSIDIAN_LINE_API_URL || 
+  (isLocalMode ? 'http://localhost:8787' : '');
+
+if (!BASE_URL) {
+  console.error('警告: OBSIDIAN_LINE_API_URLが設定されていません。APIとの通信ができません。');
+}
 
 export const API_ENDPOINTS = {
   BASE_URL,
